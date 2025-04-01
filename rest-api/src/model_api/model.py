1875ef80e9db433c3c1e182e0c2aea578e1db4b4
Norman Böttcher <norman.boettcher@web.de>
2025-04-01 19:42:59 +0200

    Added - tox.ini for build automation in rest-api

--- rest-api/src/model_api/model.py
+++ rest-api/src/model_api/model.py
@@ -7,34 +7,38 @@ from sentiment_model.text_preprocessing import ReviewPreprocessor
 import model_api.config as config
 from sentiment_model.review_learn import Attention
 
-logger = getLogger('SentimentModel')
+logger = getLogger("SentimentModel")
 
 
 class SentimentModel:
     def __init__(self):
+        self._config = config.load_config()
         self._model = self._load_model()
 
     def _load_model(self):
+        model_path = self._config["MODEL_PATH"]
         try:
-            return tf.keras.models.load_model(config.MODEL_PATH,
-                                              custom_objects={
-                                                  'Attention': Attention})
+            return tf.keras.models.load_model(
+                model_path, custom_objects={"Attention": Attention}
+            )
         except FileNotFoundError:
-            raise Exception(f'Model not found at {config.MODEL_PATH}')
+            raise Exception(f"Model not found at {model_path}")
 
     def get_model(self):
         return self._model
 
     def predict(self, review):
         if not review:
-            logger.error('Empty review detected. Returning with error.')
+            logger.error("Empty review detected. Returning with error.")
             return {"error": "Review text is empty."}
 
         try:
             lookup_table = self._get_lookup_table()
-            preprocessor = ReviewPreprocessor(lookup_table=lookup_table,
-                                              vocab_size=config.VOCAB_SIZE,
-                                              num_oov_buckets=config.NUM_OOV_BUCKETS)
+            preprocessor = ReviewPreprocessor(
+                lookup_table=lookup_table,
+                vocab_size=self._config["VOCAB_SIZE"],
+                num_oov_buckets=self._config["NUM_OOV_BUCKETS"],
+            )
             # preprocess logic needs dummy y, because funcitons are designed for inputs like x_batch, y_batch
             dummy_y = [10]
             review = tf.data.Dataset.from_tensor_slices(([review], dummy_y))
@@ -44,15 +48,16 @@ class SentimentModel:
         except RuntimeError as e:
             return {"error": str(e)}
         except Exception:
-            logger.exception('Prediction failed due to an unexpected error.')
-            return {
-                "error": "An unexpected error occurred. Please try again later."}
+            logger.exception("Prediction failed due to an unexpected error.")
+            return {"error": "An unexpected error occurred. Please try again later."}
 
     def _get_lookup_table(self):
+        table_path = self._config["LOOKUP_TABLE_PATH"]
         try:
             table_creator = LookupTableCreator()
-            return table_creator.read_from_path(config.LOOKUP_TABLE_PATH)
+            return table_creator.read_from_path(table_path)
         except Exception as e:
             logger.exception(
-                f'An error occurred reading LookupTable from {config.LOOKUP_TABLE_PATH}. ')
-            raise RuntimeError(f'Error reading lookup table: {e}')
+                f"An error occurred reading LookupTable from {table_path}."
+            )
+            raise RuntimeError(f"Error reading lookup table: {e}")
--- rest-api/tests/test_model.py
+++ rest-api/tests/test_model.py
@@ -1,10 +1,21 @@
+import importlib
+import os
 import unittest
+from unittest.mock import patch
 
+from model_api import config
 from model_api.model import SentimentModel
 
 
 class TestSentimentModel(unittest.TestCase):
+    @patch.dict(os.environ, {
+        "M7_MODEL_PATH": os.path.join(os.path.dirname(__file__), "test_resources", "Sentiment-M7.keras"),
+        "LOOKUP_TABLE_PATH": os.path.join(os.path.dirname(__file__),"test_resources", "lookup_table.json"),
+        "MODEL_VOCAB_SIZE": "10000",
+        "MODEL_NUM_OOV_BUCKETS": "1000"
+    })
     def test_load_model(self):
+        importlib.reload(config)
         model = SentimentModel()
         self.assertIsNotNone(model.get_model(), 'Error! Expected model to be not None')
 
