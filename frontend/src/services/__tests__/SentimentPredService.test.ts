614de4b638e5c435c433920aef651d46faf22375
Norman Böttcher <norman.boettcher@web.de>
2025-04-26 11:06:13 +0200

    Added - UnitTest for SentimentPredService

---
+++ frontend/src/services/__tests__/SentimentPredService.test.ts
@@ -0,0 +1,29 @@
+import { vi, it, expect, describe } from "vitest";
+import * as rest_client from "../../client/RestClient";
+import { useSentimentPredService } from "../SentimentPredService";
+
+vi.mock("axios");
+const restClientSpy = vi.spyOn(rest_client, "useRestClient");
+
+describe("negative testcases for getReviewSentiment", () => {
+  it(
+    "should throw an error if post request fails and error is not of type " +
+      "AxiosError",
+    async () => {
+      const thrownError = new Error();
+      const doPostMock = vi.fn().mockRejectedValue(thrownError);
+      restClientSpy.mockReturnValue({
+        doPost: doPostMock,
+      } as unknown as rest_client.RestClient);
+
+      const underTest = useSentimentPredService();
+
+      await expect(underTest.getReviewSentiment("some_review")).rejects.toThrow(
+        thrownError
+      );
+      expect(doPostMock).toHaveBeenCalledWith("predict", {
+        review: "some_review",
+      });
+    }
+  );
+});
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
   plugins: [react()],
@@ -7,5 +8,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './setupTests.ts'
+  },
+  resolve: {
+      alias: {
+        '@frontend': fileURLToPath(new URL('./src', import.meta.url))
+      }
   }
 })
