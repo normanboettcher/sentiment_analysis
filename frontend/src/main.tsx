cf13cc3b943401008ca621a5da906f805dbdd7ac
Norman Böttcher <norman.boettcher@web.de>
2025-05-28 19:35:31 +0200

    Changed - new Version of frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log('start up frontend with version 3.0.1')
+console.log('target host for rest-api: ', window.ENV.VITE_MODEL_API_HOST)
+console.log('target port for rest-api: ', window.ENV.VITE_MODEL_API_PORT)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
