14b993ce38d4c7a8f8484388918050d6b2e0b871
Norman Böttcher <norman.boettcher@web.de>
2025-05-29 14:46:54 +0200

    Changed - two separate ingress for backend api and SPA

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-console.log('target url for rest-api: ', window.ENV.VITE_MODEL_API_URL)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
