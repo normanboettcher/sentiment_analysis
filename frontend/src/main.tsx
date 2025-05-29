880ab23ae378fdab71faa11e374c848d684faa9a
Norman Böttcher <norman.boettcher@web.de>
2025-05-29 13:17:19 +0200

    Fixed - Build error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-console.log('start up frontend with version 3.0.1')
-console.log('target host for rest-api: ', window.ENV.VITE_MODEL_API_HOST)
-console.log('target port for rest-api: ', window.ENV.VITE_MODEL_API_PORT)
+console.log('target url for rest-api: ', window.ENV.VITE_MODEL_API_URL)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
