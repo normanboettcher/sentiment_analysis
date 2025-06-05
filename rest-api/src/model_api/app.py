99cc7cc19bcf30b45d24db07de358f73d1d29e51
Norman Böttcher <norman.boettcher@web.de>
2025-06-05 06:11:07 +0200

    Changed - added /api as parent root to flask api

--- rest-api/src/model_api/app.py
+++ rest-api/src/model_api/app.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask
 from flask_cors import CORS
-
+from flask import Blueprint
 from model_api.config.config import load_config
 from model_api.routes.metrics import metrics_bp
 from model_api.routes.review_prediction import predict_bp
@@ -19,6 +19,8 @@ def create_app(config_name=None):
     )
 
     CORS(app, origins=origins)
-    app.register_blueprint(predict_bp)
-    app.register_blueprint(metrics_bp)
+    api_bp = Blueprint('api', __name__, url_prefix='/api')
+    api_bp.register_blueprint(predict_bp)
+    api_bp.register_blueprint(metrics_bp)
+    app.register_blueprint(api_bp)
     return app
